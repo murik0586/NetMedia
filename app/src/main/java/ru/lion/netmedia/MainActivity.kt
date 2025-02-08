7f47e08d57b8e1bd2fe5034da4658e93458cb048
murik0586 <murikmusaev86@gmail.com>
2025-02-09 01:41:23 +0300

    NetMedia beta - List<Post> :)

--- app/src/main/java/ru/lion/netmedia/MainActivity.kt
+++ app/src/main/java/ru/lion/netmedia/MainActivity.kt
@@ -3,45 +3,39 @@ package ru.lion.netmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.lion.netmedia.adapter.PostAdapter
 import ru.lion.netmedia.databinding.ActivityMainBinding
-import ru.lion.netmedia.databinding.CardPostBinding
-import ru.lion.netmedia.utils.shortenNumber
+
 import ru.lion.netmedia.viewModel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by viewModels<PostViewModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        // Указываем LayoutManager для RecyclerView
+        binding.list?.layoutManager = LinearLayoutManager(this)
+        val viewModel by viewModels<PostViewModel>()
+        val adapter = PostAdapter {
+            viewModel.like(it.id)
+        }
+        binding.list?.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            posts.map { post ->
-                CardPostBinding.inflate(layoutInflater, binding.container, true).apply {
-                    author.text = post.author
-                    published.text = post.published
-                    content.text = post.content
-                    likes.text = shortenNumber(post.likes)
-                    shared.text = shortenNumber(post.shared)
-                    views.text = shortenNumber(post.views)
-                    like.setImageResource(if (post.likedByMe) R.drawable.p_thumb_up_24 else R.drawable.outline_thumb_up_24)
-                    likes.text = shortenNumber(post.likes)
-                    like.setOnClickListener {
-                        viewModel.like(post.id)
-                    }
-                }.root
+            adapter.submitList(posts)
 
-            }
+//.forEach { binding.container?.addView(it) }
 
 
         }
 
 
     }
+
     override fun onStart() {
         super.onStart()
-       // viewModel.view() TODO поправить
+        // viewModel.view() TODO поправить
     }
 }
