137f163fa555f6038ccb4a81ea6a8fad6130988a
murik0586 <murikmusaev86@gmail.com>
2025-03-10 02:40:02 +0300

    NetMedia - add buttom addPost, add fun EditPost (analogue telegram) next problems (Need fixed): focus hideKeyboard

--- app/src/main/java/ru/lion/netmedia/MainActivity.kt
+++ app/src/main/java/ru/lion/netmedia/MainActivity.kt
@@ -14,14 +14,16 @@ import ru.lion.netmedia.utils.AndroidUtils
 
 import ru.lion.netmedia.viewModel.PostViewModel
 
-class MainActivity : AppCompatActivity() {
+const val VISIBLE = View.VISIBLE
+const val GONE = View.GONE
 
+class MainActivity : AppCompatActivity() {
+    val binding = ActivityMainBinding.inflate(layoutInflater)
     val viewModel by viewModels<PostViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
-        // Указываем LayoutManager для RecyclerView
         binding.list.layoutManager = LinearLayoutManager(this)
 
         val adapter = PostAdapter(object : OnInteractionListener {
@@ -35,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onEdit(post: Post) {
                 viewModel.edit(post)
-                binding.group?.visibility = View.VISIBLE
+                binding.group?.visibility = VISIBLE
                 //TODO завершить тут тонкости
 
             }
@@ -56,9 +58,8 @@ class MainActivity : AppCompatActivity() {
         viewModel.edited.observe(this) { post ->
             if (post.id != 0L) {
                 binding.content?.requestFocus()
-                // binding.content?.setText(post.content)
                 binding.editContent?.setText(post.content)
-                binding.addPost?.visibility = View.GONE
+                binding.addPost?.visibility = GONE
             }
         }
         binding.save?.setOnClickListener {
@@ -73,24 +74,20 @@ class MainActivity : AppCompatActivity() {
             binding.content?.setText("")
             binding.content?.clearFocus()
             AndroidUtils.hideKeyboard(it)
-            binding.addPost?.visibility = View.VISIBLE
-            binding.group?.visibility = View.GONE
+            binding.addPost?.visibility = VISIBLE
+            binding.group?.visibility = GONE
             //TODO доделать чтобы клавиатура скрывалась при смене фокуса
         }
         binding.cancel?.setOnClickListener {
-            binding.group?.visibility = View.GONE
-            binding.addPost?.visibility = View.VISIBLE
+            binding.group?.visibility = GONE
+            binding.addPost?.visibility = VISIBLE
         }
         binding.addPost?.setOnClickListener {
-          binding.groupTwo?.visibility = View.VISIBLE
-            binding.addPost.visibility = View.GONE
+            binding.groupTwo?.visibility = VISIBLE
+            binding.addPost.visibility = GONE
         }
 
 
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//        // viewModel.view() TODO поправить
-//    }
 }
